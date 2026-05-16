f9acbab17bdbe37bc2745b6659254893a416a694
Mark <you@example.com>
2026-05-17 02:45:49 +0800

    fix smtp timeout settings

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,11 +1,10 @@
 import nodemailer from 'nodemailer';
 
 export default async function sendEmail({ to, subject, html }: any) {
-
     try {
-
         console.log('SMTP HOST:', process.env.SMTP_HOST);
         console.log('SMTP USER:', process.env.SMTP_USER);
+        console.log('SMTP PORT:', process.env.SMTP_PORT);
 
         const transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
@@ -14,7 +13,10 @@ export default async function sendEmail({ to, subject, html }: any) {
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS
-            }
+            },
+            connectionTimeout: 10000,
+            greetingTimeout: 10000,
+            socketTimeout: 10000
         });
 
         const info = await transporter.sendMail({
@@ -24,11 +26,9 @@ export default async function sendEmail({ to, subject, html }: any) {
             html
         });
 
-        console.log('EMAIL SENT:', info);
+        console.log('EMAIL SENT:', info.messageId);
 
     } catch (error) {
-
         console.log('EMAIL ERROR:', error);
-
     }
 }
