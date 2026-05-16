f97f344c37df55e2f02d675f080b4f52522490c4
Mark <you@example.com>
2026-05-16 22:38:50 +0800

    added email logging

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -2,20 +2,30 @@ import nodemailer from 'nodemailer';
 
 export async function sendEmail({ to, subject, html }: any) {
 
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
-        secure: false,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
-        }
-    });
+    try {
 
-    await transporter.sendMail({
-        from: `"Mark Remitar" <${process.env.SMTP_USER}>`,
-        to,
-        subject,
-        html
-    });
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: Number(process.env.SMTP_PORT),
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
+            }
+        });
+
+        const info = await transporter.sendMail({
+            from: `"Mark Remitar" <${process.env.SMTP_USER}>`,
+            to,
+            subject,
+            html
+        });
+
+        console.log('EMAIL SENT:', info);
+
+    } catch (error) {
+
+        console.log('EMAIL ERROR:', error);
+
+    }
 }
